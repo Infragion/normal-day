062b54e23db8fbc50c74e3987cea7e4bc349dd6f
Egor Chernov <51438050+Infragion@users.noreply.github.com>
2024-08-23 01:10:08 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -181,7 +181,7 @@ app.get("/db", async (req, res) => {
   const raw = req.headers.cookie.split('.');
   const username = hex_to_ascii(raw[0].toString())
   const hash = raw[1].toString().concat('.', raw[2].toString()).toString()
-  if (hash === await kv.get(`${username}:user`)) {
+  if (hash == await kv.get(`${username}:user`)) {
     const keys = await kv.keys("*")
     let code = "<head> <script src='/db_client.js'></script> </head> <input id='sinput' style='position: absolute; top: 1%; right: 6%;' placeholder='Username' type='text'> <button id='search' style='position: absolute; top: 1%; right: 1%;'>Search</button>"
     for (var key in keys) {

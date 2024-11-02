20aec08ae0d947114882f77d5dd93be052dbec9b
Egor Chernov <51438050+Infragion@users.noreply.github.com>
2024-11-02 23:51:43 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -384,7 +384,7 @@ app.post('/db/upd', async (req, res) => {
   if (req.headers.cookie === undefined) {res.status(401).json( {error: 401, message: "Unathorized"}); return 0}
   const raw = req.headers.cookie.split('.');
   const username = raw[0].toString()
-  const hash = raw[2].toString()
+  const hash = raw[1].toString()
 
   if (hash === await kv.get(`${username}:user`)) {
     try {

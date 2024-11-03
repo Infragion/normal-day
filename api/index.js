6f47938685199e33b915576417d282f7cd676785
Egor Chernov <51438050+Infragion@users.noreply.github.com>
2024-11-03 12:37:12 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -382,6 +382,7 @@ app.get('/db/upd', async (req, res) => {
 
 app.post('/db/upd', async (req, res) => {
   if (req.headers.cookie === undefined) {res.status(401).json( {error: 401, message: "Unathorized"}); return 0}
+  console.log(req.headers.cookie);
   const raw = req.headers.cookie.split('.');
   const username = raw[0].toString()
   const hash = `${raw[1].toString()}.${raw[2].toString()}`
